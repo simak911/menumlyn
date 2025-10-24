6d852c7665df4492ba2d7c9946ecc5bf8b124021
Lucien Šíma <lucien.sima@aristotelos.com>
2025-10-24 09:10:43 +0200

    adding date checker

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request
 import requests
 from waitress import serve
+from bs4 import BeautifulSoup
+from datetime import datetime
+import pytz
+
+def get_weekday():
+    try:
+        timezone = "Europe/Prague"
+        url = f"https://worldtimeapi.org/api/timezone/{timezone}"
+        response = requests.get(url)
+        data = response.json()
+        return data["day_of_week"]
+    except:
+        timezone = "Europe/Prague"
+        tz = pytz.timezone(timezone)
+        now = datetime.now(tz)
+        return now.isoweekday()
 
 app = Flask(__name__)
 @app.route('/')
@@ -12,9 +28,11 @@ def get_main_page():
         response.raise_for_status()
     except:
         text = "Došlo k chybě."
-        return render_template ('index.html', ukaz="chyba")
-    html = response.text
-    return render_template('index.html', ukaz=html)
+        return render_template ('index.html', chyba="chyba")
+    string_html = response.text
+    s = BeautifulSoup(string_html, "html.parser")
+    wd = get_weekday()
+    return render_template('index.html', chyba=f"{wd}")
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
